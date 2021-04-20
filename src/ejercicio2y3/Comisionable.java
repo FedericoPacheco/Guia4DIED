ecaa1fa11c064736509309285e8ea7ebb51cea9b
FedericoPacheco <fedepacheco2112@gmail.com>
2021-04-20 09:27:49 -0300

    terminado

--- src/ejercicio2y3/Comisionable.java
+++ src/ejercicio2y3/Comisionable.java
@@ -2,6 +2,7 @@ package ejercicio2y3;
 
 public interface Comisionable 
 {
+	// Se interpretan las comisiones como porcentajes. Los montos adicionales son simplemente dinero.
 	public abstract Double getComisionServicio();
 	public abstract Double getMontoAdicionalServicio();
 	public abstract Double getComisionCadete();
